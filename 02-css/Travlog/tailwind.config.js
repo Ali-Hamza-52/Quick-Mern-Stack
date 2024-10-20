4c3ec810faa25b89b8bf8ab3b1b361d9f34fbaa4
ali-hamza-52 <alihamzabrw52@gmail.com>
2024-10-20 15:39:12 +0500

    travlog web ui

---
+++ 02-css/Travlog/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+ content: ["./src/**/*.{html,js}"],
+
+  theme: {  
+    extend: {
+      colors: {
+        pinkish: "#f85e9f"
+      }
+    },
+  },
+  plugins: [],
+}
+
