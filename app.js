9c92dd98be7f14d83d6d2905f46b59df63ecaffe
ali-hamza-52 <alihamzabrw52@gmail.com>
2024-11-09 17:33:49 +0500

    day 7 js

---
+++ app.js
@@ -0,0 +1,98 @@
+// 3 ways to display value
+  /*
+          console.log(value);
+          alert("This is alert")
+          document.write()
+      */
+  //Variables
+  /*
+          How to declare variables
+              let = change value when we need
+              var = same as above
+              const = Never Changed
+
+              //declaration
+              let variableName;
+
+              // Value Assignments
+              variableName = value;
+
+              //Initialization
+              let variableName = value;
+
+              // Constant initialization
+              const VARIABLE_NAME = value;
+
+              // Variables Naming Rule
+              let _name = "Ali";
+              let $name = "Ali";
+              let name = "Ali";
+              let NAME = "Ali";
+              let name1 = "Ali";
+              let firstName = "Ali";
+
+              //We Cannot use Reserved Keywords
+
+              // Reserved are reserved by language we cannot use it as a
+              variable name
+
+              // Examples
+              let let = "Ali";
+              let var = "Ali";
+              let return = "Ali";
+              let break = "Ali";
+              let continue = "Ali";
+              let if = "Ali";
+              let else = "Ali";
+              let switch = "Ali";
+              let while = "Ali";
+              let do = "Ali";
+              let for = "Ali";
+              let in = "Ali";
+              let typeof = "Ali";
+              let finally = "Ali";
+      */
+  // Data Types
+  /*
+          String : let name = "ali";
+          Number : let age = 22;
+          Boolean : let isTeenage = true;
+          Null : let address = null;
+          Undefined : let age;
+          Symbol : let symbol = Symbol("Greeting");
+          Object : const person = {name: "ali", age: 22};
+
+          let name = "Ali";
+          let age = 25;
+          let isMarried = true;
+          let address = null;
+          let greeting;
+          let symbol = Symbol("Greeting");
+          let person = {
+              name: "Ali",
+              age: 25,
+              address: {
+                  street: "Street",
+                  city: "City"
+              }
+          };
+      */
+
+  // Type Coercion (Type Casting)
+  /*
+      // String to Number
+          let name = "25";
+          let age = Number(name);
+          console.log(age);
+
+          // Number to String
+          let age = 25;
+          let name = String(age);
+          let name1 = age.toString();
+          console.log(name);
+
+          // Boolean to Number
+          let isMarried = true;
+          let isTeenage = Boolean(isMarried);
+          console.log(isTeenage);
+      */
