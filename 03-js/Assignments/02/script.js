9951d4db118debb04bdfe2fad1bb2036dfd2d814
ali-hamza-52 <alihamzabrw52@gmail.com>
2024-11-16 18:15:06 +0500

    assignment 2 solutions

---
+++ 03-js/Assignments/02/script.js
@@ -0,0 +1,76 @@
+// let number = parseInt(prompt("Enter a number"));
+
+// if (number >= 1 && number <= 10) {
+//   if (number === 1) {
+//     console.log(`The ${number} in roman version is = I `);
+//   } else if (number === 2) {
+//     console.log(`The ${number} in roman version is = II `);
+//   } else if (number === 3) {
+//     console.log(`The ${number} in roman version is = III `);
+//   } else if (number === 4) {
+//     console.log(`The ${number} in roman version is = IV `);
+//   } else if (number === 5) {
+//     console.log(`The ${number} in roman version is = V `);
+//   } else if (number === 6) {
+//     console.log(`The ${number} in roman version is = VI `);
+//   } else if (number === 7) {
+//     console.log(`The ${number} in roman version is = VII `);
+//   } else if (number === 8) {
+//     console.log(`The ${number} in roman version is = VIII `);
+//   } else if (number === 9) {
+//     console.log(`The ${number} in roman version is = IX `);
+//   } else {
+//     console.log(`The ${number} in roman version is = X `);
+//   }
+// } else {
+//   console.log("Please Enter Valid Number between 1 - 10");
+// }
+
+// let day = parseInt(prompt("Enter Days"));
+// let month = parseInt(prompt("Enter month"));
+// let year = parseInt(prompt("Enter year"));
+
+// let isMagical = day * month === year;
+
+// if (isMagical) {
+//   console.log("Date is magical");
+// } else {
+//   console.log("Date is not magical");
+// }
+
+// let lastMonthChecks = Number(prompt("Enter last month checks"));
+
+// let fee = 10.0;
+// if (lastMonthChecks >= 0) {
+//   if (lastMonthChecks < 20) {
+//     fee = fee + lastMonthChecks * 0.1;
+//   } else if (lastMonthChecks >= 20 && lastMonthChecks <= 39) {
+//     fee = fee + lastMonthChecks * 0.08;
+//   } else if (lastMonthChecks >= 40 && lastMonthChecks <= 59) {
+//     fee = fee + lastMonthChecks * 0.06;
+//   } else {
+//     fee = fee + lastMonthChecks * 0.04;
+//   }
+// } else {
+//   console.log("Don't allow negitive number");
+// }
+
+let num1 = Math.floor(Math.random() * 40) + 10;
+let num2 = Math.floor(Math.random() * 40) + 10;
+
+// document.write(`<h1 style="color:blue;">The first numbers is ${num1}</h1>`);
+// document.write(`<br />`);
+// document.write(`<h1 style="color:blue;">The Second number is ${num2}</h1>`);
+// document.write(
+//   `<h1 style="color:blue;">Calculate the sum of following and answer </h1>`
+// );
+
+console.log("num1 is ",num1)
+console.log("num2 is ",num2)
+let sum = Number(prompt("Enter the sum of random generated number"));
+
+if ((num1 + num2) == sum) {
+  alert("Congralations your answer is right");
+} else {
+  alert("Oops your answer is wrong");
+}
