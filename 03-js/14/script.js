6a353c70e7678d4852d4919a5bac487b9874359c
ali-hamza-52 <alihamzabrw52@gmail.com>
2024-11-25 15:40:42 +0500

    revise if else if

---
+++ 03-js/14/script.js
@@ -0,0 +1,43 @@
+// let isBlast = confirm("Are you sure to blast your computer?");
+
+// if(isBlast){
+//     console.log("Blasting started!");
+// }else{
+//    console.log("Blasting cancelled!");
+// }
+
+/* 
+    How to make conditions
+
+    relational and logical operator
+
+    Relational Operator
+        compare two values (6 > 7)
+        > < >= <= != === ==
+    Logical Operator
+        compare two relational equations (5>6) && (6<9)
+        && || !
+
+    Condition must have at least one relational operators
+        we can do    
+            if(2 > 3)
+
+            taking number from user and should be greater than 2 and less then 5
+            if(number>2)
+                if(number<5)
+            if(number > 2 && number < 5)
+
+        if(num) // it is wrong when it have number
+        if("user type value") // it is wrong when it have string
+        if(isBlast) // it is good when it have boolean(true ,false)
+        if(number === 1) // it is true
+        if(string === "hi") // it is correct
+        if("hi") // it is wrong
+
+
+    if (condition) {
+        Code to run if condition is true
+    } else {
+        Code to run if condition is false
+    }
+*/
